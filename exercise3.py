8998e1d9cc9bae409a28e39cdfa7777106976af1
Abdul Rehman <abdulrehmanfayyaz123@gmail.com>
2025-10-29 02:49:13 +0100

    added tolerance

--- exercise3.py
+++ exercise3.py
@@ -31,8 +31,8 @@ class Rectangle:
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return self.is_in_interval(ll_px, self._dx, tolerance) \
-            and self.is_in_interval(ll_py, self._dy, tolerance)
+        return  ll_px >= -tolerance and ll_px <= self._dx + tolerance \
+            and ll_py >= -tolerance and ll_py <= self._dy + tolerance
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
